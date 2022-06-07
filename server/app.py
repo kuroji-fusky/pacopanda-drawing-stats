c6b227210bd6134d873d6e5767a4fdc259fe9a6f
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2022-06-07 12:02:22 +0800

    adding optional args

--- server/app.py
+++ server/app.py
@@ -4,21 +4,34 @@ Written by Kerby Keith Aquino (skepfusky / Kokoro Husky)
 MIT License
 """
 
+import argparse
 import uvicorn
 import json
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 
-app = FastAPI(debug=True)
+parser = argparse.ArgumentParser(description="Paco Drawing Stats FastAPI Back-end written in Python")
+
+parser.add_argument("--prod", action="store_true", help="Run the build/production app")
+
+args = parser.parse_args()
+config = vars(args)
+print(config)
+
+app = FastAPI()
+
+@app.get("/")
+def main():
+  return {"message": "test"}
 
 @app.get("/artwork")
 def artwork():
     artwork_data: object = json.load("paco-fa-database.json")
     return artwork_data
 
-@app.get("/characters")
-def characters():
-    character_data: object = json.load("characters-example.json")
+@app.get("/character")
+def character():
+    character_data: object = json.load("character-example.json")
     return character_data
 
 if __name__ == "__main__":
-    uvicorn.run("fastapi_code:app")
+    uvicorn.run("app:app", reload=True)
