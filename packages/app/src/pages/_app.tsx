74fa3215c306ea017d9efa5d08ad79432c37effa
Kerby Keith Aquino <skepfoosky15@gmail.com>
2022-11-30 12:33:59 +0000

    ✔️ Minor changes for successful build

--- packages/app/src/pages/_app.tsx
+++ packages/app/src/pages/_app.tsx
@@ -2,7 +2,7 @@ import { Layout } from "@/components/Base";
 import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
 
-export default function PDS({ Component, pageProps }: AppProps) {
+export default function PacoPandaDrawingStats({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Component {...pageProps} />
