24a2557904054a339bf861fed4e2fd4eb77dff28
thatITfox <maxthecomputerfox@gmail.com>
2022-12-17 15:24:00 +0700

    Added redisDB connected

--- python/scraper/utils.py
+++ python/scraper/utils.py
@@ -1,4 +1,6 @@
 from colorama import Back, Fore, Style
+import redis
+import json
 
 """For adding fancy colors to the console"""
 clear = Style.RESET_ALL
@@ -16,3 +18,30 @@ def warn_msg(msg: str):
 
 def error_msg(msg: str):
     print(f"{f_red}{msg}{clear}")
+
+def connect_redis(filepath: str):
+    print("Connecting to redis...")
+    try:
+        with open(filepath, "r") as f:
+            config = json.load(f)
+    
+        cfg_host: str = config["host"]
+        cfg_port: int = int(config["port"])
+        cfg_username: str = config["username"]
+        cfg_password: str = config["password"]
+
+        if cfg_password is None:
+            cfg_password = ''
+
+        RedisDB = redis.StrictRedis(
+            host=cfg_host,
+            port=cfg_port,
+            username=cfg_username,
+            password=cfg_password,
+            decode_responses=True
+        )
+    # i'm gonna go take a tea break, brb kuro :3
+    # k lol
+    except FileNotFoundError:
+        print("file not found")
+    return RedisDB
