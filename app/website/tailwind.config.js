40bb98b0294670c19e98891aad69aa33a571128f
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2023-01-23 13:53:41 +0800

    🔧 Add search history store & base tailwind config

--- app/website/tailwind.config.js
+++ app/website/tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const basePlugin = require("tailwindcss/plugin")
+
 module.exports = {
 	content: [
 		"./components/**/*.{js,vue,ts}",
@@ -9,7 +11,27 @@ module.exports = {
 		"./app.vue"
 	],
 	theme: {
+		fontFamily: {
+			"inter": ["Inter"],
+			"open-sans": ["Open Sans"]
+		},
 		extend: {}
 	},
-	plugins: [require("@tailwindcss/typography")]
+	plugins: [
+		basePlugin(({ addBase, theme }) => {
+			addBase({
+				html: {
+					"scrollBehavior": "smooth",
+					"overflowX": "hidden",
+					"@media (prefers-reduced-motion)": {
+						scrollBehavior: "auto"
+					}
+				},
+				body: {
+					fontFamily: theme("fontFamily.open-sans")
+				}
+			})
+		}),
+		require("@tailwindcss/typography")
+	]
 }
