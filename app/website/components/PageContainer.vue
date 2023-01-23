9caa458f158177c4281911b94e13f3e8fd704b85
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2023-01-23 13:17:18 +0800

    ♻️ Reflow folder structure; add tailwindcss

---
+++ app/website/components/PageContainer.vue
@@ -0,0 +1,35 @@
+<template>
+	<main>
+		<slot />
+	</main>
+</template>
+
+<script setup lang="ts">
+const router = useRoute()
+
+const URL_PATH = `httos://pds.kurofusky.xyz${router.fullPath}`
+
+const props = defineProps<{
+	title: string
+	description?: string
+	contentWrap?: boolean
+}>()
+
+useHead({
+	title: props.title,
+	meta: [
+		{ name: "description", content: props.description },
+		{ name: "twitter:title", content: props.title },
+		{ name: "twitter:description", content: props.description },
+		{ name: "twitter:url", content: URL_PATH },
+		{ name: "twitter:card", content: "summary_large_card" },
+		{ name: "twitter:creator", content: "@kurojifusky" },
+		{ property: "og:title", content: props.title },
+		{ property: "og:description", content: props.description },
+		{ property: "og:type", content: "website" },
+		{ property: "og:url", content: URL_PATH },
+		{ property: "og:site_name", content: "Paco Drawing Stats" }
+	],
+	link: [{ rel: "canonical", href: URL_PATH }]
+})
+</script>
---
+++ app/website/components/TheBaseNavbar.vue
@@ -0,0 +1,5 @@
+<template>
+	<header></header>
+</template>
+
+<style scoped></style>
