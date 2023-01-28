486da1e3e14eb9f935f9c29f283718ec45544118
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2023-01-29 00:46:26 +0800

    ✨ return fontsource

--- scripts/cleanBloat.js
+++ scripts/cleanBloat.js
@@ -1,8 +1,8 @@
 import fs from "fs"
 
-const dirs = [".", "./apps/website", "./apps/api", "./puppeteer"]
+const nodeDirs = [".", "./apps/website", "./apps/api", "./puppeteer"]
 
-dirs.forEach((dir) => {
+nodeDirs.forEach((dir) => {
 	const prependNode = `${dir}/node_modules`
 
 	console.log(`Deleting ${prependNode}...`)
