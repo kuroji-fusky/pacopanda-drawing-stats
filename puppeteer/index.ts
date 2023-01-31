afdec8c0613fbe5001ce65518d1ceda10c38e6fa
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2023-01-31 16:10:27 +0800

    ⚙️ puppeteer: add recusive func

--- puppeteer/index.ts
+++ puppeteer/index.ts
@@ -1,19 +1,44 @@
-const BASE_URL = "https://www.furaffinity.net/gallery/pacopanda/"
-
 import puppeteer from "puppeteer"
+import fs from "fs"
+
+const args = [
+	"--disable-background-networking",
+	"--disable-background-timer-throttling",
+	"--no-first-run",
+	"--no-sandbox"
+]
+
+const BASE_URL = "https://www.furaffinity.net/gallery/pacopanda/"
+let TOTAL_PAGES = 1
+
 ;(async () => {
-	const browser = await puppeteer.launch()
-	const navigatePage = await browser.newPage()
+	const browser = await puppeteer.launch({
+		args: args
+	})
 
-	const galleryPage = await navigatePage.goto(BASE_URL)
+	const page = await browser.newPage()
 
-    /**
-     * TODO This page uses pagination.
-     * TODO recursively find the "Next" button until the element doesn't anymore,
-     * TODO append it to a variable and then go through the usual stuff
-     */
-    let totalPages = 0;
+	await page.goto(BASE_URL)
 
-	// This line should be kept at the bottom at all times unless an exception has thrown.
+	const isNext = await page.evaluate(() => {
+		TOTAL_PAGES = 1
+
+		// prettier-ignore
+		const query = `.submission-list form[action="/gallery/pacopanda/${TOTAL_PAGES+1}/"]`
+		return document.querySelector(query)
+	})
+
+	while (isNext) {
+		let url = `${BASE_URL}${TOTAL_PAGES}/?`
+		await page.goto(url)
+
+		TOTAL_PAGES++
+
+		let query = `.submission-list form[action="/gallery/pacopanda/${TOTAL_PAGES}/"]`
+		await page.$eval(query, (e) => console.log(e))
+
+		console.log("Found page", TOTAL_PAGES)
+	}
+
 	await browser.close()
 })()
