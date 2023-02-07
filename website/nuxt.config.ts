f8e841a61374725d92bea010458ca70e3adc682d
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2023-02-07 16:14:16 +0800

    📊 add microsoft clarity

--- website/nuxt.config.ts
+++ website/nuxt.config.ts
@@ -15,6 +15,22 @@ export default defineNuxtConfig({
 		],
 	],
 	plugins: [{ src: "~/plugins/vercel.ts", mode: "client" }],
+	app: {
+		head: {
+			script: [
+				{
+					// prettier-ignore
+					children: `
+          (function(c,l,a,r,i,t,y){
+          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "${process.env.CLARITY_ID ?? 'NOT_SET'}");
+          `,
+				},
+			],
+		},
+	},
 	build: {
 		transpile: ["gsap"],
 	},
@@ -36,9 +52,5 @@ export default defineNuxtConfig({
 				],
 			}),
 		],
-	},
-	typescript: {
-		strict: true,
-		shim: false,
 	},
 })
