50c070e015f5dbee6b9da1ddfe9d4aca74c038ea
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2023-02-07 16:22:23 +0800

    🗑️ remove services component

--- website/components/Landing/LandingServices.vue
+++ website/components/Landing/LandingServices.vue
@@ -11,43 +11,5 @@
 				but just enough to get this ambious project out there!
 			</p>
 		</article>
-		<ul class="services-list-grid">
-			<LandingServicesItem
-				v-for="service in SERVICES_LIST"
-				:name="service.service"
-				:logo="service.logo"
-				:description="service.purpose"
-			/>
-		</ul>
 	</section>
 </template>
-
-<script setup lang="ts">
-import Railway from "../Icons/Railway.vue"
-
-const SERVICES_LIST = [
-	{
-		service: "Vercel",
-		link: "https://vercel.com/home",
-		purpose: "To host this website and the APIs (yes it's possible!)",
-	},
-	{
-		service: "Redis Enterprise Cloud",
-		link: "https://redis.com",
-		purpose:
-			"For providing a Redis database (via the free tier) from a Google Cloud vendor",
-	},
-	{
-		service: "Railway",
-		link: "https://railway.app",
-		logo: Railway,
-		purpose: "To host a Docker container to update the data on a daily basis",
-	},
-]
-</script>
-
-<style lang="scss">
-.services-list-grid {
-	@apply grid grid-flow-col grid-cols-3 gap-5;
-}
-</style>
