8c49f9e9cbfdfe460f774e11a3d66beda47371f8
Kerby Keith Aquino <skepfoosky15@gmail.com>
2023-02-17 19:39:45 +0800

    📦 add auto-import functionality on admin app

--- apps/admin-vite/src/App.vue
+++ apps/admin-vite/src/App.vue
@@ -1,7 +1,3 @@
-<script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue"
-</script>
-
 <template>
 	<div>
 		<a href="https://vitejs.dev" target="_blank">
@@ -11,7 +7,7 @@ import HelloWorld from "./components/HelloWorld.vue"
 			<img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
 		</a>
 	</div>
-	<HelloWorld msg="Vite + Vue" />
+	<HelloWorld msg="Vite + LOL" />
 </template>
 
 <style scoped>
