4eb1ca073315722cfc887d82a284ac7383278b7d
Kerby Keith Aquino <skepfoosky15@gmail.com>
2023-02-18 13:35:28 +0800

    Revert "Merge branch 'main' of https://github.com/kuroji-fusky/pacopanda-drawing-stats into main"
    This reverts commit e8ae7e42dd0f75d6a0ebb374d0b191add68043e1, reversing
    changes made to 56e20868d020f465725e27b130463997fef0c7b9.

--- apps/admin-vite/tailwind.config.cjs
+++ apps/admin-vite/tailwind.config.cjs
@@ -1,9 +1,5 @@
-const sharedConfig = require("@pds/config/tailwind.config.cjs")
 /** @type {import('tailwindcss').Config} */
-<<<<<<< HEAD
 const sharedConfig = require("pds-custom-config/tailwind.config.cjs")
-=======
->>>>>>> ac36948b229c1d8efe2b014c22dd8f1f4fab20e9
 
 module.exports = {
 	content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
