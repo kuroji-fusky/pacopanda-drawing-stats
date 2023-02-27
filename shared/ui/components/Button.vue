eea1e585294a63f9a2420554a21370f76abe163d
Kerby Keith Aquino <skepfoosky15@gmail.com>
2023-02-27 18:44:20 +0800

    🎨 init `shared/ui` directory

---
+++ shared/ui/components/Button.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineProps<{ name?: string }>()
+</script>
+
+<template>
+	<button type="button">
+		{{ name }}
+	</button>
+</template>
+
+<style lang="scss" scoped>
+button {
+	@apply px-1.5 py-3 rounded-md border-black;
+}
+</style>
---
+++ shared/ui/nuxt.mjs
@@ -0,0 +1,16 @@
+import { join, dirname } from "path"
+import { fileURLToPath } from "url"
+import { defineNuxtModule } from "@nuxt/kit"
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+export default defineNuxtModule({
+	hooks: {
+		"components:dirs"(dirs) {
+			dirs.push({
+				path: join(__dirname, "components"),
+				prefix: "PDS",
+			})
+		},
+	},
+})
